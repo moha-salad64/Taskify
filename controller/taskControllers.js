df3738f2cf543d1b4e4e7f7cd180bc0c4d36c136
moha-salad64 <mohamedsalaad509@gmail.com>
2024-11-06 14:45:09 +0300

    updating controller functionality

--- controller/taskControllers.js
+++ controller/taskControllers.js
@@ -45,7 +45,53 @@ exports.createTask = (req , res) =>{
 }
 
 exports.updateTask = (req , res) => {
-    res.end(JSON.stringify({message: 'Task updated successfully'}))
+    const form = new IncomingForm();
+    form.parse(req , (err , fields , files) => {
+
+        if(err) {
+            res.writeHead(400 , {'content-type': 'application/json'});
+            res.end(JSON.stringify({message: 'Error parsing form'}));
+            return;
+        }
+        if(!fields.title){
+            res.writeHead(400 , {'content-type': 'application/json'});
+            res.end(JSON.stringify({message: 'Title is required'})); 
+            return;
+        }
+
+        const image = files.image ? files.image[0] : null;
+        const tasks = readTasksfromFile();
+        const taskId  = parseInt(req.url.split('/').pop());
+        const taskIndex = tasks.findIndex(task => task.id === taskId);
+
+        if(taskIndex === -1){
+            res.writeHead(404 , {'content-type': 'application/json'});
+            res.end(JSON.stringify({message: 'Any task not found'}));
+
+            return;
+        }
+
+        const updatedTask = {
+            ...tasks[taskIndex],
+            title: fields.title || tasks[taskIndex].title,
+            description: fields.description || tasks[taskIndex].description,
+            status: fields.status || tasks[taskIndex].status,
+            image: image ? `/uploads/${image.originalFilename}` : tasks[taskIndex].image,
+        }
+
+        tasks[taskIndex] = updatedTask;
+        writeTasktoFile(tasks);
+
+        if(image) {
+            copyFileSync(image.filepath, path.join(__dirname, '../uploads' , image.originalFilename));
+        }
+
+        res.writeHead(200 , {'content-type': 'application/json'});
+        res.end(JSON.stringify(updatedTask))
+
+      
+    })
+    
 }
 
 exports.deleteTask = (req , res) => {
